077977bf91bc564f9d558f0aae73290f8fb49313
Luis Aguilar <laz133@example.com>
2023-02-13 20:09:07 -0500

    Creada aplicacion

---
+++ recetas_compras/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecetasComprasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recetas_compras'
---
+++ recetas_compras/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Semana(models.Model):
+    nombre = 
+    created_at = 
