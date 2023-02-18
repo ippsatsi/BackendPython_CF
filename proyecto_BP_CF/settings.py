7db0bc0a5fb5205177e3d1311fca4f8feb2e6715
Luis Aguilar <laz133@example.com>
2023-02-18 18:56:57 -0500

    Modelos terminados

--- proyecto_BP_CF/settings.py
+++ proyecto_BP_CF/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recetas_compras'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'proyecto_BP_CF.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['recetas_compras/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recetas_compras/models.py
+++ recetas_compras/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
+
 class Ingrediente(models.Model):
     TYPE_UNIDAD = [
         ("PEN", "Soles"),
@@ -18,7 +19,7 @@ class Ingrediente(models.Model):
         ("ESP", "Especias"),
         ("LAC", "Lacteos"),
         ("OVO", "Huevos"),
-        ('FRU', 'Frutas')
+        ('FRU', 'Frutas'),
         ("OTR", "Otros"),
     ]
     nombre = models.CharField(max_length=100)
@@ -32,7 +33,13 @@ class Ingrediente(models.Model):
         choices= TYPE_UNIDAD,
         default='UN'
     )
-    created_at = models.DateTimeField(default=timezone.now)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        pass
 
 
 class Receta(models.Model):
@@ -42,7 +49,7 @@ class Receta(models.Model):
         ("CER", "Cerdo"),
         ("MAR", "Marino"),
         ("OVO", "Huevos"),
-        ('CON', 'Conservas')
+        ('CON', 'Conservas'),
         ("OTR", "Otros"),
     ]
     nombre = models.CharField(max_length=100)
@@ -50,16 +57,37 @@ class Receta(models.Model):
         max_length= 3,
         choices= TYPE_INGREDIENTE_PRINCIPAL,
     )
-    ingredientes = models.ManyToManyField(Ingrediente, related_name= 'get_ingredientes',  through='M2M_Recetarios')
+    ingredientes = models.ManyToManyField(Ingrediente, related_name= 'get_ingredientes',  through='M2M_Recetario')
     image = models.ImageField(upload_to="recetas_compras/photos")
     preparacion = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
 
-class M2M_Recetarios(models.Model):
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        pass
+        
+
+class M2M_Recetario(models.Model):
     ingrediente = models.ForeignKey(Ingrediente, on_delete= models.PROTECT)
     receta = models.ForeignKey(Receta, on_delete= models.CASCADE)
     cantidad = models.DecimalField(max_digits=3, decimal_places=1, )
 
-class Semana(models.Model):
-    pass
+    def __str__(self):
+        return f'{self.receta} - {self.ingrediente}'
 
+    class Meta:
+        pass
+
+
+class Semana(models.Model):
+    nombre = models.CharField(max_length=100)
+    recetas = models.ManyToManyField(Receta, related_name= 'get_recetas')
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        pass
