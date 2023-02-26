746ccb93da6c78ad3f30b91bd026d8485cdd2758
Luis Aguilar <laz133@example.com>
2023-02-25 21:00:33 -0500

    Creando formset para Receta ...

--- recetas_compras/forms.py
+++ recetas_compras/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 from django.forms import ModelForm, inlineformset_factory
 from .models import M2MRecetario, Receta, Ingrediente
+
+
+class AgregarIngredienteForm(ModelForm):
+
+    class Meta:
+        model = M2MRecetario
+        fields =
