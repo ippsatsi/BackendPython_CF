54816f80a0498f85e035a04b5a3970d91f226137
ippsatsi <laz133@gmail.com>
2023-03-05 17:45:22 -0500

    Cambiando DB a MySql

--- proyecto_BP_CF/settings.py
+++ proyecto_BP_CF/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'proyecto_BP_CF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recetas_compras',
+        'USER': 'luis',
+        'PASSWORD': 'Configuraci0n',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
